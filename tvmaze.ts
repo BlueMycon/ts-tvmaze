e540015b244d1af1d498d66aed20fe1dfab06fae
Steven <stzheng716@gmail.com>
2023-06-05 12:00:30 -0700

    add part 1 and 2

--- tvmaze.ts
+++ tvmaze.ts
@@ -1,11 +1,18 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import * as $ from 'jquery';
 
-const $showsList = $("#showsList");
-const $episodesArea = $("#episodesArea");
-const $searchForm = $("#searchForm");
+const $showsList:JQuery = $("#showsList");
+const $episodesArea:JQuery = $("#episodesArea");
+const $searchForm:JQuery = $("#searchForm");
+const BASE_URL:string = "https://api.tvmaze.com/search/shows" 
+const default_image
 
-
+interface showInterface {
+  id: number,
+  name: string,
+  summary: string,
+  image?: string
+}
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
@@ -13,40 +20,27 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term: string):Promise<[showInterface]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const response: AxiosResponse = await axios(`${BASE_URL}/${term}`)
+
+  return response.data;
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: [showInterface]): void{
   $showsList.empty();
+
+
 
   for (let show of shows) {
     const $show = $(
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src="${show.image}"
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
